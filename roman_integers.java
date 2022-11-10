e3b77524487f54f8a8ac102d4d350c32e191d031
Tyroneous <52468054+Tyroneous9@users.noreply.github.com>
2022-11-10 15:49:59 -0600

    Update roman_integers.java

--- roman_integers.java
+++ roman_integers.java
@@ -2,29 +2,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 class roman_integers {
-    Map<String, Integer> romanInts = new HashMap<String, Integer>();
-    roman_integers() {
-        romanInts.put("I", 1);
-        romanInts.put("V", 5);
-        romanInts.put("X", 10);
-        romanInts.put("L", 50);
-        romanInts.put("C", 100);
-        romanInts.put("D", 500);
-        romanInts.put("M", 1000);
-    }
     
     public int romanToInt(String s) {
+        Map<Character, Integer> romanInts = new HashMap<Character, Integer>();
+        romanInts.put('I', 1);
+        romanInts.put('V', 5);
+        romanInts.put('X', 10);
+        romanInts.put('L', 50);
+        romanInts.put('C', 100);
+        romanInts.put('D', 500);
+        romanInts.put('M', 1000);
         int sum = 0;
-        int len = s.length();
-        sum += romanInts.get(s.substring(0, 1));
-        if(len<=1)
-            return sum;
-        int prev, curr;
-        for(int i=1; i<len; i++) {
-            prev = romanInts.get(s.substring(i-1, i));
-            curr = romanInts.get(s.substring(i, i+1));
-            if(prev<curr)
-                sum += prev - curr;
+        char[] arr = s.toCharArray();
+        int curr, next;
+        for(int i=0; i<arr.length; i++) {
+            curr = romanInts.get(arr[i]);
+            if(i==arr.length-1) 
+                return sum += curr;
+            next = romanInts.get(arr[i+1]);
+            if(curr<next) {
+                sum += next-curr;
+                i += 1;
+            }
             else
                 sum += curr;
         }
@@ -32,6 +31,6 @@ class roman_integers {
     }
 
     public static void main(String[] args) {
-        System.out.println(new roman_integers().romanToInt("XVI"));
+        System.out.println(new roman_integers().romanToInt("XXVII"));
     }
 }
