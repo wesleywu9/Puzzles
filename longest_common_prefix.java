db620f1e292c816b53796bfb416a9cba4f397657
Wesley <wesleywu9@gmail.com>
2022-11-29 12:22:48 -0600

    Update longest_common_prefix.java

--- longest_common_prefix.java
+++ longest_common_prefix.java
@@ -1,3 +1,12 @@
 public class longest_common_prefix {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = "";
+        
+        return prefix;
+    }
     
+    public static void main() {
+        String[] a = {"abc", "ab"};
+
+    }
 }
