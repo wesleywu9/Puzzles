40f16e8b975caae3f811106a4381f277a68bbf2d
Wesley <wesleywu9@gmail.com>
2023-01-10 19:49:59 -0600

    a

--- longest_common_prefix.java
+++ longest_common_prefix.java
@@ -1,18 +1,38 @@
-// Example 1:
-// Input: strs = ["flower","flow","flight"]
-// Output: "fl"
+/*
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl" 
+*/
 
 public class longest_common_prefix {
     public static String longestCommonPrefix(String[] strs) {
-        char[] prefix = strs[0].toCharArray();
-        for(int i=1; i<strs.length; i++) {
-            
+        int maxLen=strs[0].length(), len;
+        for(String str : strs) {
+            len = str.length();
+            if(len<maxLen)
+                maxLen = len;
         }
-        return prefix;
+        StringBuilder prefix = new StringBuilder();
+        int strIdx=0, charIdx=0;
+        while(charIdx < maxLen) {
+            boolean testPassed = true;
+            char test = strs[0].charAt(charIdx);
+            for(strIdx=1; strIdx<strs.length; strIdx++) {
+                if(strs[strIdx].charAt(charIdx) != test) {
+                    testPassed = false;
+                    break;
+                } 
+            }
+            if(testPassed == false)
+                break;
+            prefix.append(test);
+            charIdx++;
+        }
+        return prefix.toString();
     }
-    
-    public static void main() {
-        String[] input = {"abc", "ab"};
-        System.out.println(longest_common_prefix.longestCommonPrefix(input));
+    public static void main(String args[]) {
+        String[] input = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(input));
     }
 }
+
---
+++ valid_parentheses.java
@@ -0,0 +1,17 @@
+/*
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+*/
+
+public class valid_parentheses {
+    
+}
